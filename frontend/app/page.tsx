9375772c8fbf8eb06771ccc50f45bf3f732d6d19
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-09-04 17:56:28 +0800

    chore(Poomo): automated sync - 1 modified
    Automated commit to sync local changes.
    Timestamp: 2025-09-04 09:56:28 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - frontend/app/page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,9 @@ import Timer from "@/components/timer/Timer";
 import Player from "@/components/player/Player";
 export default function home() {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <Timer></Timer>
-      <Player></Player>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Timer />
+      <Player />
     </div>
   );
 }
