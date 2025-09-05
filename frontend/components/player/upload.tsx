543ccad077a854d4737249c6d6e532dec63f7972
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-09-05 10:11:56 +0800

    chore(Poomo): automated sync - 1 modified
    Automated commit to sync local changes.
    Timestamp: 2025-09-05 02:11:56 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - frontend/components/player/upload.tsx

--- frontend/components/player/upload.tsx
+++ frontend/components/player/upload.tsx
@@ -77,7 +77,7 @@ export default function uploadFile() {
       <Button
         onClick={handleButtonClick}
         disabled={uploading}
-        className="px-3 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 disabled:opacity-60"
+        className="px-3 py-1 bg-blue-900 text-white rounded-lg hover:bg-blue-600 disabled:opacity-60"
       >
         {uploading ? "Uploading..." : <Upload></Upload>}
       </Button>
