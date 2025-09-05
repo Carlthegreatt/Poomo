b4f35c7763c6f31453da32668036da151ce9dbef
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-09-06 01:35:16 +0800

    chore(Poomo): automated sync - 1 modified
    Automated commit to sync local changes.
    Timestamp: 2025-09-05 17:35:16 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - frontend/components/header/header.tsx

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -6,8 +6,8 @@ import { CircleUser } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
-      <div className="container flex h-14 items-center justify-between px-4">
+    <header className="sticky flex items-center justify-center top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
+      <div className="container flex h-14 justify-between items-center  px-4">
         <div className="font-semibold text-xl">Poomo</div>
         <div className="flex items-center gap-4">
           <Button variant="ghost" size="icon">
