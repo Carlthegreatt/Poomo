5d85d9f999a73edfd928424e7b6fd775055828ff
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-09-06 01:50:16 +0800

    chore(Poomo): automated sync - 1 modified
    Automated commit to sync local changes.
    Timestamp: 2025-09-05 17:50:16 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - frontend/components/header/header.tsx

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="sticky flex items-center justify-center top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
       <div className="container flex h-18 justify-between items-center px-4">
-        <div className="font-semibold text-2xl">Poomo</div>
+        <div className="font-semibold text-xl lg:text-2xl">Poomo</div>
         <div className="flex items-center gap-4">
           <Button className="cursor-pointer" variant="ghost" size="icon">
             <ChartBar className="cursor-pointer h-4 w-4" />
